741d6406960156ad317122b31584d187a3fc8f67
rai <mj82xx@gmail.com>
2022-08-26 22:53:48 +0900

    window setting test

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
+import { Form } from "./components/Form";
 
 function App() {
-	return (
-		<div>Hello</div>
-	)
+  return (
+    <>
+      <div>Hello</div>
+      <Form />
+    </>
+  );
 }
 
 export default App;
