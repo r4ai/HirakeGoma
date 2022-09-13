0c939866fcdd6da56d121387609e17394e152a7f
e97 <e4zy971@gmai.com>
2022-09-13 10:42:28 +0900

    sync: color mode setting

--- src/setting_window/setting.tsx
+++ src/setting_window/setting.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript, DarkMode } from "@chakra-ui/react";
 import { Router } from "./components/Router";
+import theme from "./Theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <Router />
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <DarkMode>
+        <Router />
+      </DarkMode>
     </ChakraProvider>
   </React.StrictMode>
 );
