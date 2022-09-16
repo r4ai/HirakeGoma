aa625fb6d7d2833b78c5b05d1db95396e746ef2a
e97 <e4zy971@gmai.com>
2022-09-16 23:03:35 +0900

    feat: setting_win; bg_color

--- src/setting_window/components/Layout.tsx
+++ src/setting_window/components/Layout.tsx
@@ -1,13 +1,19 @@
 import { FC, ReactNode, useState } from "react";
-import { VStack, Grid, GridItem, Box } from "@chakra-ui/react";
+import { VStack, Grid, GridItem, Box, useColorModeValue } from "@chakra-ui/react";
 import { TitleBar } from "./organisms/TitleBar";
 import { SideBar } from "./organisms/SideBar";
 import { Outlet } from "react-router-dom";
+import { title } from "process";
 
 export type navSize = "small" | "large";
 
 export const Layout: FC = () => {
   const [navSize, setNavSize] = useState<navSize>("large");
+
+  const bg = useColorModeValue("white", "black");
+  const navBg = useColorModeValue("gray.200", "gray.800");
+  const titleBg = useColorModeValue("gray.100", "gray.900");
+  const mainBg = useColorModeValue("gray.100", "gray.900");
 
   return (
     <>
@@ -17,15 +23,14 @@ export const Layout: FC = () => {
         gridTemplateRows={"20px 1fr"}
         gridTemplateColumns={`${navSize === "large" ? "150px" : "30px"} 1fr`}
         h="100vh"
-        bg={"blackAlpha.900"}
       >
-        <GridItem area={"title"} bg="orange.300">
+        <GridItem area={"title"} bgColor={titleBg}>
           TitleBar
         </GridItem>
-        <GridItem area={"nav"} bg="pink.300">
+        <GridItem area={"nav"} bgColor={navBg}>
           <SideBar navSize="large" />
         </GridItem>
-        <GridItem area={"main"} bg="green.300" px={5} py={2}>
+        <GridItem area={"main"} bgColor={mainBg} px={5} py={2}>
           <Outlet />
         </GridItem>
       </Grid>
