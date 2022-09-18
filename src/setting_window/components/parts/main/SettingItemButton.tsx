58f3185c2f70eb3688a202647e446e26ca20ace3
e97 <e4zy971@gmai.com>
2022-09-18 21:03:00 +0900

    feat: new component; SettingItemButton

--- src/setting_window/components/parts/main/SettingItemButton.tsx
+++ src/setting_window/components/parts/main/SettingItemButton.tsx
@@ -1,1 +1,10 @@
-import { FC } from "react";
+import { Button, ButtonProps } from "@chakra-ui/react";
+import { FC, ReactElement } from "react";
+
+interface Props extends ButtonProps {
+  children?: ReactElement;
+}
+
+export const SettingItemButton: FC<Props> = ({ children, ...props }) => {
+  return <Button {...props}>{children}</Button>;
+};
