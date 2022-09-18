3f5f80b9c0d69942fc48c1a39e08c307c8b2d28b
e97 <e4zy971@gmai.com>
2022-09-19 00:04:46 +0900

    refactor: pages folder 構成

--- src/setting_window/components/Router.tsx
+++ src/setting_window/components/Router.tsx
@@ -2,13 +2,7 @@ import { FC } from "react";
 import { HashRouter, Routes, Route, BrowserRouter } from "react-router-dom";
 
 import { Layout } from "./Layout";
-import { CorePlugins } from "./pages/core_plugins";
-import { Database } from "./pages/database";
-import { Debug } from "./pages/debug/debug";
-import { General } from "./pages/general";
-import { Hotkey } from "./pages/hotkey";
-import { Plugin } from "./pages/plugin";
-import { Theme } from "./pages/theme";
+import { Debug, Database, General, Hotkey, Plugin, Theme } from "./pages";
 
 export const Router: FC = () => {
   return (
@@ -18,7 +12,6 @@ export const Router: FC = () => {
           <Route path="/" element={<Layout />}>
             <Route index element={<General />}></Route>
             <Route path="/general" element={<General />}></Route>
-            <Route path="/core_plugins" element={<CorePlugins />}></Route>
             <Route path="/plugin" element={<Plugin />}></Route>
             <Route path="/database" element={<Database />}></Route>
             <Route path="/theme" element={<Theme />}></Route>
