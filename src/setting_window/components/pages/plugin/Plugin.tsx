82931627f9c0914becc5c1b8732b1c875408fb1a
e9716 <mj82xx@gmail.com>
2022-11-23 00:31:10 +0900

    feat: setting/plugin/appsearchの、アイテムを一部並び変えた

--- src/setting_window/components/pages/plugin/Plugin.tsx
+++ src/setting_window/components/pages/plugin/Plugin.tsx
@@ -7,8 +7,8 @@ export const Plugin: FC = () => {
   return (
     <>
       <SettingHeading title="Application Search" />
-      <Generate />
       <FolderPaths />
+      <Generate />
       <UploadToMainTable />
       <Debug />
     </>
