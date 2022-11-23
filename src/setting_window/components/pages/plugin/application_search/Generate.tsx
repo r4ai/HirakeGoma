df0b1de1f41ef877097db67990f39408742a14fa
e9716 <mj82xx@gmail.com>
2022-11-23 10:50:22 +0900

    feat: add suspence for setting/appsearch/generate

--- src/setting_window/components/pages/plugin/application_search/Generate.tsx
+++ src/setting_window/components/pages/plugin/application_search/Generate.tsx
@@ -1,17 +1,37 @@
-import { FC } from "react";
+import { useToast } from "@chakra-ui/react";
+import { FC, useState } from "react";
 
 import { generateIndex } from "../../../../../commands/plugin/appsearch/generateIndex";
 import { SettingItem, SettingItemButton } from "../../../parts/main";
 
 export const Generate: FC = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const errorToast = useToast();
+
   async function handleClick(): Promise<void> {
-    await generateIndex();
+    setIsLoading(true);
+    generateIndex()
+      .then((s) => {
+        setIsLoading(false);
+      })
+      .catch((e) => {
+        errorToast({
+          title: "ERROR",
+          description: "Failed to generate indexes in application table."
+        });
+      });
   }
 
   return (
     <>
       <SettingItem title="Generate" description="Generate index in application table.">
-        <SettingItemButton onClick={handleClick}>Generate</SettingItemButton>
+        {isLoading ? (
+          <SettingItemButton isLoading loadingText="Generating" onClick={handleClick}>
+            Generate
+          </SettingItemButton>
+        ) : (
+          <SettingItemButton onClick={handleClick}>Generate</SettingItemButton>
+        )}
       </SettingItem>
     </>
   );
